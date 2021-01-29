882df02e92102d50078a3a55ed6a8b15dc5abf83
DestinyDiez <destiny.diezdeandino@patelhs.org>
2021-01-29 14:04:13 +0000

    i decided to start off with scanner first to get it out of the way. I then added scanner and made it to where you print your username and it would output it

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ class Main {
   public static void main(String[] args) {
     Scanner myUser = new Scanner(System.in);
     String userName;
-    
-    
+  
     System.out.println("Hello and welcome to Discord please Enter username!"); 
     userName = myUser.nextLine();   
        
